2282a0f940cdc70cda7a098582cd14b1ee281a96
skj4244 <skj4244@naver.com>
2023-10-30 03:06:55 +0900

    refactor: Restaurant API format

--- backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/Restaurant/RestaurantController.java
+++ backend/src/main/java/com/SeeAndYouGo/SeeAndYouGo/Restaurant/RestaurantController.java
@@ -6,9 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
